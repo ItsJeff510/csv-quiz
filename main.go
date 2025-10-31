02512cd4946f4d5986ca1b38a4d7d504ca476608
Jovanny Ramos <jovannyr72@gmail.com>
2025-10-31 12:10:55 -0700

    Golang Quizzer v0.1

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println("Error reading CSV file:", err)
+		return
+	}
+
+	fmt.Println("You will be taking a quick math quiz.\n")
+
+	count := 0
+	correct := 0
+	var i int
+
+	for _, record := range records {
+		count++
+		fmt.Println("Question", count, ": ")
+		fmt.Println(record[0], "=")
+		fmt.Scan(&i)
+
+		answer, err := strconv.Atoi(record[1])
+		if err != nil {
+			fmt.Println("Error converting answer to integer:", err)
+			continue
+		}
+		if i == answer {
+			correct++
+		}
+	}
+
+	fmt.Printf("You got %d out of %d correct.\n", correct, count)
+	fmt.Printf("You scored : %.2f%%\n", (float64(correct)/float64(count))*100)
+}
